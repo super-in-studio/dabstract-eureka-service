e1a22e03c8bc3f635123883158a94d6fb27c9355
dickens.hu <848422116@qq.com>
2019-12-05 22:16:24 +0800

    log config

--- src/main/java/com/dabstract/base/DabstractEurekaServiceApplication.java
+++ src/main/java/com/dabstract/base/DabstractEurekaServiceApplication.java
@@ -1,5 +1,7 @@
 package com.dabstract.base;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -10,6 +12,7 @@ public class DabstractEurekaServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DabstractEurekaServiceApplication.class, args);
+		
 	}
 
 }
